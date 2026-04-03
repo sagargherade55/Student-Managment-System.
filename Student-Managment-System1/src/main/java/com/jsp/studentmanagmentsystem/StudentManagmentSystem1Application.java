50cebb17b8c7669fc919b610718356bd6dda698b
sagargherade55 <sagargherade55@gmail.com>
2026-04-03 23:02:39 +0530

    initial commit

---
+++ Student-Managment-System1/src/main/java/com/jsp/studentmanagmentsystem/StudentManagmentSystem1Application.java
@@ -0,0 +1,13 @@
+package com.jsp.studentmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentSystem1Application.class, args);
+	}
+
+}
---
+++ Student-Managment-System1/src/test/java/com/jsp/studentmanagmentsystem/StudentManagmentSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.studentmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
